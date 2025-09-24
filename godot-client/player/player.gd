52ae6697e8eaad183dd93827429ac3b03b723dca
Andrew Davis <jonandrewdavis@gmail.com>
2025-09-24 15:39:14 -0400

    chore: upload initial poc

---
+++ godot-client/player/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -400.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _process(delta: float) -> void:
+	var mouse_direction = (get_global_mouse_position() - global_position).normalized()
+	if mouse_direction.x > 0 and animated_sprite.flip_h:
+		animated_sprite.flip_h = false
+	elif mouse_direction.x < 0 and not animated_sprite.flip_h:
+		animated_sprite.flip_h = true
+
+	if velocity.x > 0:
+		animated_sprite.play('walk')
+	else:
+		animated_sprite.play('idle')
