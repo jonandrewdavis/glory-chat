e677fdbc485cf1d6834a2ec9ed6add13fd186cc4
Andrew Davis <jonandrewdavis@gmail.com>
2025-09-28 16:36:24 -0400

    feat: must play the video before the game loads

--- godot-client/main.gd
+++ godot-client/main.gd
@@ -3,11 +3,14 @@ extends Node
 var game_world = preload("res://world/world.tscn")
 
 func _ready():
-	# Game start signal
-	if not OS.has_feature('admin'):
-		LobbySystem.signal_network_create_new_peer_connection.connect(new_game_connection)
-	else:
+	# All start in windowed mode, so twitch can play first.
+	get_window().mode = Window.MODE_WINDOWED
+
+	# Game start signal - only admins will connect on game start.
+	if OS.has_feature('admin'):
 		LobbySystem.signal_lobby_game_started.connect(new_game_connection)
+	else:
+		LobbySystem.signal_network_create_new_peer_connection.connect(new_game_connection)
 
 func new_game_connection(_id):
 	# TODO: Improve. This is fragile.
